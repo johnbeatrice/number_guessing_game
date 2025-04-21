bf5b92504c4500fd3186307d1b813c11a219c2bc
John Beatrice <johncbeatrice@gmail.com>
2025-04-21 22:39:35 +0000

    feat: everything except the random number generator

--- build_db.sh
+++ build_db.sh
@@ -3,7 +3,7 @@
 # run this file to build the number_guessing_game database
 
 # connect to postgres database
-PSQL1="psql --username=freecodecamp --dbname=postgres"
+PSQL1="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 # connect to number_guess database
 PSQL2="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
@@ -13,3 +13,4 @@ echo "$($PSQL1 "CREATE DATABASE number_guess;")"
 # create player_info table
 echo "$($PSQL2 "CREATE TABLE player_info(player_id SERIAL PRIMARY KEY, username VARCHAR(22) UNIQUE NOT NULL, games_played INT, best_game INT);")"
 
+echo "$($PSQL2 "INSERT INTO player_info(username) VALUES ('john');")"
--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,110 @@
 
 # number guessing game
 
+# connect to number_guess database
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# variables
+declare player_info_string
+declare -a player_info_arr
+
+declare current_guesses=0
+declare username_is_valid=0
+declare player_guess_bool=0
+
+declare hide_update_db_output
+
+# # get current player info
+# player_info_string=echo "$($PSQL1 "SELECT * FROM player_info WHERE username = '$USERNAME';")"
+
+#  IFS='|'
+#   read -ra player_info_arr <<< $player_info_string
+#   unset IFS
+
+
+get_user_info () {
+
+  echo "Enter your username:"
+
+  while [ $username_is_valid == 0 ]
+  do
+    read USERNAME
+    if [[ -z $USERNAME || ${#USERNAME} -gt 22 ]];
+    then
+      echo "Please enter a valid username. That user name may be taken."
+    else
+      username_is_valid=1
+    fi
+  done
+
+  # check if user is in database
+  player_info_string="$($PSQL "SELECT * FROM player_info WHERE username = '$USERNAME';")"
+
+  if [[ ! -z $player_info_string ]];
+    then
+      # retrieve all player info
+      IFS='|'
+      read -ra player_info_arr <<< $player_info_string
+      unset IFS
+
+      echo -e "\nWelcome back, ${player_info_arr[1]}! You have played ${player_info_arr[2]} games, and your best game took ${player_info_arr[3]} guesses."
+      
+      # echo $player_info_string
+      # echo ${player_info_arr[@]}
+    else
+      echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+    fi
+}
+
+guessing_game () {
+  
+  echo -e "\nGuess the secret number between 1 and 1000:"
+
+  # generate secret number here, assign to var secret_number
+  secret_number=10
+
+  while [ $player_guess_bool == 0 ]
+  do
+    read PLAYER_GUESS
+    # increment current_guesses by 1 before if condition
+    ((++current_guesses))
+    if [[ ! $PLAYER_GUESS =~ ^[+-]?[0-9]+$ ]];
+    then
+      echo "That is not an integer, guess again:"
+
+    elif [[ $PLAYER_GUESS -gt $secret_number ]];
+    then
+      echo "It's lower than that, guess again:" 
+
+    elif [[ $PLAYER_GUESS -lt $secret_number ]];
+    then
+      echo "It's higher than that, guess again:" 
+    else
+      player_guess_bool=1
+      echo "You guessed it in $current_guesses tries. The secret number was $secret_number. Nice job!"
+    fi
+  done
+
+# update user info in player_info database
+if [[ -z $player_info_string ]];
+then
+  echo "$($PSQL "INSERT INTO player_info(username, games_played, best_game) VALUES ('$USERNAME', 1, $current_guesses);")" | $hide_update_db_output
+
+elif [[ $current_guesses -lt ${player_info_arr[3]} ]];
+then
+  echo "$($PSQL "UPDATE player_info SET games_played = games_played + 1, best_game = $current_guesses WHERE username = '${player_info_arr[1]}';")" | $hide_update_db_output
+
+else
+  echo "$($PSQL "UPDATE player_info SET games_played = games_played + 1 WHERE username = '${player_info_arr[1]}';")" | $hide_update_db_output
+fi
+
+
+}
+
+# program start
+ echo -e "\n~~~~ Number Guessing Game ~~~~\n"
+ get_user_info
+ guessing_game
 
 # When you run your script, you should prompt the user for a username with "Enter your username:", 
 # and take a username as input. Your database should allow usernames that are 22 characters
@@ -12,6 +116,7 @@
 # and <best_game> being the fewest number of guesses it took that user to win the game
 
 # If the username has not been used before, you should print "Welcome, <username>! It looks like this is your first time here."
+
 
 # The next line printed should be "Guess the secret number between 1 and 1000:" and input from the user should be read
 
